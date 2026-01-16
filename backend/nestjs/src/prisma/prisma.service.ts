efb27f05a147d612cf039b34d0d988f4e92f179f
chenjianzhong <jianzhong.chen@qq.com>
2026-01-16 19:03:40 +0800

    feat: prisma 日志

--- backend/nestjs/src/prisma/prisma.service.ts
+++ backend/nestjs/src/prisma/prisma.service.ts
@@ -1,13 +1,33 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Injectable, Logger } from '@nestjs/common';
+import { PrismaClient, Prisma } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient<
+  Prisma.PrismaClientOptions,
+  'query'
+> {
+  private readonly logger = new Logger(PrismaService.name);
   constructor() {
     const adapter = new PrismaPg({
       connectionString: process.env.DATABASE_URL,
     });
-    super({ adapter });
+    const isProduction = process.env.NODE_ENV === 'production';
+    super({
+      adapter,
+      log: isProduction ? [] : [{ emit: 'event', level: 'query' }],
+    });
+    if (!isProduction) {
+      this.$on('query', (event: Prisma.QueryEvent) => {
+        // query: SQL text, params: bound parameters, duration: execution time in ms.
+        // this.logger.log(
+        //   '[prisma]',
+        //   event.query,
+        //   event.params,
+        //   `${event.duration}ms`,
+        // );
+        this.logger.log(event);
+      });
+    }
   }
 }
